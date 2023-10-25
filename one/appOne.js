ec1ac02c34629788fc466556ac7bf36d7e99b30b
techsaheb <techsaheb.com@gmail.com>
2023-10-25 05:15:52 +0300

    first commit

---
+++ one/appOne.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function(button){
+    button.addEventListener("click", function(e){
+       
+
+        if(e.target.id == 'grey'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id == 'purple'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id == 'blue'){
+            body.style.backgroundColor = "blue"
+        }
+        if(e.target.id == 'yellow'){
+            body.style.backgroundColor = "yellow"
+        }
+    })
+})
