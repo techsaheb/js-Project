8d53fa9dc0602581de54400b2bd5d2a812ec8cb6
techsaheb <techsaheb.com@gmail.com>
2023-10-26 04:01:36 +0300

    add notion link

--- appOne.js
+++ appOne.js
@@ -1,21 +1,33 @@
+
+// we add all span tag class name button in a variable return a node list
 const buttons = document.querySelectorAll(".button");
+// body also store in a variable
 const body = document.querySelector("body");
 
+// buttons is a node list thatwhy i use for each
 buttons.forEach(function(button){
-    button.addEventListener("click", function(e){
-       
 
-        if(e.target.id == 'grey'){
+    // this line for event manaege
+    button.addEventListener('click', function(e){
+        
+       // then now condition
+        if(e.target.id == "grey"){
+            body.style.backgroundColor = "grey"
+        }
+        if(e.target.id == "purple"){
             body.style.backgroundColor = e.target.id
         }
-        if(e.target.id == 'purple'){
+        
+        if(e.target.id == "blue"){
             body.style.backgroundColor = e.target.id
         }
-        if(e.target.id == 'blue'){
-            body.style.backgroundColor = "blue"
-        }
-        if(e.target.id == 'yellow'){
-            body.style.backgroundColor = "yellow"
+        if(e.target.id == "yellow"){
+            body.style.backgroundColor = e.target.id
         }
     })
-})
+
+});
+
+
+
+
